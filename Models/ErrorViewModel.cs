33f0ee198008f1452d1b0f38f837cf3cdd8b333d
1111s <a1111@1111ss-MacBook-Air.local>
2025-12-11 23:38:01 +0700

    Init project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VillaManagementWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
